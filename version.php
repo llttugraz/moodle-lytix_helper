8d24671baa8b4cb24505a65822365f7616e44eb5
Guenther Moser <moser@tugraz.at>
2023-11-28 09:38:18 +0100

    v1.1.4

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ $plugin->component = 'lytix_helper'; // Full name of the plugin.
 $plugin->dependencies = [
     'lytix_logs' => ANY_VERSION
 ];
-$plugin->release   = 'v1.1.3';
+$plugin->release   = 'v1.1.4';
 $plugin->supported = [401, 401];
