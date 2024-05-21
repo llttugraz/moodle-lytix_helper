78cb2029f135474aa3ee3bf0aa8629d046792794
Alex Kremser <145429704+llt-tuggy@users.noreply.github.com>
2024-05-21 10:26:54 +0200

    prepare latest release

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023100300; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->requires  = 2022112800.00; // Requires this Moodle version 4.1.
-$plugin->component = 'lytix_helper'; // Full name of the plugin.
+$plugin->version = 2024052101;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->requires = 2022112800.00; // Requires this Moodle version 4.1.
+$plugin->component = 'lytix_helper';
 $plugin->dependencies = [
-    'lytix_logs' => ANY_VERSION
+    'lytix_logs' => ANY_VERSION,
 ];
-$plugin->release   = 'v2.0.0';
+$plugin->release = 'v2.0.3';
 $plugin->supported = [402, 403];
